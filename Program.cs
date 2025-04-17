d4f6e22371fe9912818ad266e770438e55ff44ed
AJ Cunha <argenisjc@hotmail.com>
2025-04-17 16:48:00 -0500

    First Commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace wk3ex3_Review;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str;
+        int vowels = 0;
+
+        Console.Write("Enter a string: ");
+        str = (Console.ReadLine().ToLower());
+
+        foreach (char c in str)
+        {
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+            {
+                vowels++;
+
+            }
+        }
+
+        Console.WriteLine($"The Number of vowels in the string is: {vowels}");
+
+        Console.Read();
+
+    }
+}
+
