7f4bb96ddaeff7ca5f96b068ad0636f1ef9b2620
AJ Cunha <argenisjc@hotmail.com>
2025-04-21 06:09:16 -0500

    Eof message added

--- Program.cs
+++ Program.cs
@@ -3,19 +3,28 @@ class Program
 {
     static void Main(string[] args)
     {
+        //Initialize the string variable
         string str;
+        //Initialize and store the vowel count
         int vowels = 0;
+        //pormpt the user for the string
         Console.Write("Enter a string: ");
+        //stores the string value
         str = (Console.ReadLine().ToLower());
-
+        //foreach loop to iterate the string, for each character in string
         foreach (char c in str)
         {
+            //if the characters == to vowels increase the vowel count
             if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
             {
+                //increase the vowel count by one
                 vowels++;
             }
         }
+        //display the number of vowels
         Console.WriteLine($"The Number of vowels in the string is: {vowels}");
+        //stops the program to allow th user to read the result
+        Console.WriteLine("Press any key to exit...");
         Console.Read();
     }
 }
